d72217de164e7483a8f49bfde0e3678e08a17c3d
Shahib Hasan <knotstorybd@gmail.com>
2026-05-02 06:00:04 +0600

    fix: dynamically redirect back to original page after google login

--- src/app/store/useStore.ts
+++ src/app/store/useStore.ts
@@ -450,7 +450,7 @@ export const useStore = () => {
   const signInWithGoogle = async () => {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
-      options: { redirectTo: window.location.origin }
+      options: { redirectTo: window.location.href }
     });
     if (error) throw error;
   };
